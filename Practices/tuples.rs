2e5fa62f48aa67c7aa8f025d08a6008c05b02b5f
Imamul Hassan <imamul.hassan@bjitgroup.com>
2022-05-26 15:17:47 +0600

    tupples with dot operator.
    Signed-off-by: Imamul Hassan <imamul.hassan@bjitgroup.com>

--- Practices/tuples.rs
+++ Practices/tuples.rs
@@ -3,6 +3,9 @@ fn main() {
 
     let (x, y, z) = tup;
 
+    let a = tup.0;
+    println!("The value of a is: {}", a);
+
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
     println!("The value of z is: {}", z);
